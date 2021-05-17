b4515eb8417ed2d8674f337db2c7ecbef1284a7f
Rémy Raes <contact@remyraes.com>
2021-05-17 11:39:45 +0200

    [test] clusterPaths should throw with no input paths

--- test/tests/clustering.test.ts
+++ test/tests/clustering.test.ts
@@ -1,6 +1,6 @@
 import {clusterPaths} from "../../src/clustering";
 import {flandersRuns} from "../features/runs";
-const expect = require('chai').expect;
+import { expect } from "chai";
 
 describe ('clustering', () => {
     it ('should return some paths', () => {
@@ -11,5 +11,11 @@ describe ('clustering', () => {
     it ('should not return input paths', () => {
         const clusteredPaths = clusterPaths(flandersRuns);
         expect(clusteredPaths).to.not.deep.equal(flandersRuns);
+    });
+
+    it ('should throw with no input paths', () => {
+        expect(
+            () => clusterPaths([])
+        ).to.throw(RangeError, 'Input paths array must contain at least two paths.');
     });
 });
