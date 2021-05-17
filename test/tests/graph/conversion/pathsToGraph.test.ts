46864887d1f4747c7d33f1aacbdfb6eb25d3f0a8
Rémy Raes <contact@remyraes.com>
2021-05-17 16:30:04 +0200

    [test] pathsToGraph should create a graph with several sub-graphs

--- test/tests/graph/conversion/pathsToGraph.test.ts
+++ test/tests/graph/conversion/pathsToGraph.test.ts
@@ -1,7 +1,9 @@
 import {pathsToGraph} from "../../../../src/graph/conversion/pathsToGraph";
-import {mouaisRun, testRun} from "../../../features/runs";
+import {flandersRuns, mouaisRun, testRun} from "../../../features/runs";
 import {expect} from "chai";
 import {PointNode} from "../../../../src/graph/PointNode";
+const hash = require('object-hash');
+
 
 describe ('pathsToGraph', () => {
     it ('should create as many nodes as path points', () => {
@@ -19,5 +21,13 @@ describe ('pathsToGraph', () => {
             const pathPosition = pathPositions[index];
             expect(node.point.geometry.coordinates).to.deep.equal(pathPosition);
         });
+    });
+
+    it ('should create a graph with several sub-graphs', () => {
+        const graph = pathsToGraph(flandersRuns);
+        for (const inputPath of flandersRuns) {
+            const subGraph = graph.getSubGraphStartingFrom(hash(inputPath.geometry.coordinates[0]));
+            expect(subGraph.getNodes().length).to.deep.equal(inputPath.geometry.coordinates.length);
+        }
     });
 });
