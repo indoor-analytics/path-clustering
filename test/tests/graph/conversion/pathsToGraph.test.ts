9d8db251237e7f6cccfd91a15291e96683a4bc10
Rémy Raes <contact@remyraes.com>
2021-05-17 15:47:52 +0200

    [test] pathsToGraph should create a graph with same nodes order as input path

--- test/tests/graph/conversion/pathsToGraph.test.ts
+++ test/tests/graph/conversion/pathsToGraph.test.ts
@@ -1,10 +1,23 @@
 import {pathsToGraph} from "../../../../src/graph/conversion/pathsToGraph";
-import {mouaisRun} from "../../../features/runs";
+import {mouaisRun, testRun} from "../../../features/runs";
 import {expect} from "chai";
+import {PointNode} from "../../../../src/graph/PointNode";
 
 describe ('pathsToGraph', () => {
     it ('should create as many nodes as path points', () => {
         const graph = pathsToGraph([mouaisRun]);
         expect(graph.getNodes().length).to.equal(mouaisRun.geometry.coordinates.length);
+    });
+
+    it ('should create a graph with same nodes order as input path', () => {
+        const pathPositions = testRun.geometry.coordinates;
+
+        const graph = pathsToGraph([testRun]);
+        const nodes = graph.topologicallySortedNodes();
+
+        nodes.forEach((node: PointNode, index: number) => {
+            const pathPosition = pathPositions[index];
+            expect(node.point.geometry.coordinates).to.deep.equal(pathPosition);
+        });
     });
 });
