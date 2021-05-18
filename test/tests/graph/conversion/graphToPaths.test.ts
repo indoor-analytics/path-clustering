cfc89c366b6ae8de6a7a06eeeff2e7b0277a7a56
Rémy Raes <contact@remyraes.com>
2021-05-18 12:06:36 +0200

    [test] graphToPaths should return one path

--- test/tests/graph/conversion/graphToPaths.test.ts
+++ test/tests/graph/conversion/graphToPaths.test.ts
@@ -2,6 +2,8 @@ import {PointNode} from "../../../../src/graph/PointNode";
 import {expect} from "chai";
 import {graphToPaths} from "../../../../src/graph/conversion/graphToPaths";
 import {DirectedAcyclicGraph} from "typescript-graph";
+import {sha1} from "object-hash";
+import {point} from "@turf/helpers";
 
 describe ('graphToPaths', () => {
     it ('should throw with graph having no nodes', () => {
@@ -9,5 +11,20 @@ describe ('graphToPaths', () => {
         expect(
             () => graphToPaths(graph)
         ).to.throw(Error, 'Input graph must feature nodes.');
+    });
+
+    it ('should return one path', () => {
+        const graph = new DirectedAcyclicGraph<PointNode>();
+        const point1 = [0, 1],
+            point2 = [1, 2],
+            point3 = [2, 3];
+        const node1 = graph.insert({hash: sha1(point1), point: point(point1)}),
+            node2 = graph.insert({hash: sha1(point2), point: point(point2)}),
+            node3 = graph.insert({hash: sha1(point3), point: point(point3)});
+        graph.addEdge(node1, node2);
+        graph.addEdge(node2, node3);
+
+        const paths = graphToPaths(graph);
+        expect(paths.length).to.equal(1);
     });
 });
