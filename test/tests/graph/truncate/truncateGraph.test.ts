1497f1378efefe02872316495a013506e5c40f3d
Rémy Raes <raes.remy@gmail.com>
2021-05-19 19:21:47 +0200

    [test] adding truncateGraph specification tests

---
+++ test/tests/graph/truncate/truncateGraph.test.ts
@@ -0,0 +1,9 @@
+describe ('truncateGraph', () => {
+    xit ("should return input graph if it's strictly included in zone");
+    xit ('should truncate a graph with a path beginning outside zone');
+    xit ('should truncate a graph with a path ending outside zone');
+    xit ('should truncate a graph with a path going outside, then inside zone');
+    xit ('should truncate a graph with a path both starting and ending outside zone');
+    xit ('should truncate a graph with a path crossing zone border several times');
+    xit ('should throw if entire graph is outside zone');
+});
