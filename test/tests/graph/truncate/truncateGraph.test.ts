7841712509e440f01931a71e088f6873e9aaac24
Rémy Raes <raes.remy@gmail.com>
2021-05-20 11:54:58 +0200

    [test] truncateGraph should return input graph if it's strictly included in zone

--- test/tests/graph/truncate/truncateGraph.test.ts
+++ test/tests/graph/truncate/truncateGraph.test.ts
@@ -1,5 +1,17 @@
+import {pathsToGraph} from "../../../../src/graph/conversion/pathsToGraph";
+import {flandersRuns} from "../../../features/runs";
+import {truncateGraph} from "../../../../src/graph/truncate/truncateGraph";
+import {flandersStation} from "../../../features/zones";
+import { expect } from "chai";
+
 describe ('truncateGraph', () => {
-    xit ("should return input graph if it's strictly included in zone");
+    // https://gist.github.com/Alystrasz/ed61bac17ab98373986c5eae54b798ed
+    it ("should return input graph if it's strictly included in zone", () => {
+        const graph = pathsToGraph(flandersRuns());
+        const truncatedGraph = truncateGraph(graph, flandersStation());
+        expect(truncatedGraph).to.deep.equal(graph);
+    });
+
     xit ('should truncate a graph with a path beginning outside zone');
     xit ('should truncate a graph with a path ending outside zone');
     xit ('should truncate a graph with a path going outside, then inside zone');
