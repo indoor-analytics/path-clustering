fbe884322fbae42bbf8f87705622107fc12ae483
Rémy Raes <raes.remy@gmail.com>
2021-05-27 17:06:19 +0200

    [feat] adding intersection nodes insertion skeleton

--- src/graph/truncate/truncateGraph.ts
+++ src/graph/truncate/truncateGraph.ts
@@ -17,6 +17,7 @@ export function truncateGraph (
 
     let hasNodesInsideZone = false;
 
+    // marking nodes outside zone
     for (const node of graph.getNodes()) {
         const result = booleanPointInPolygon(node.point, zone);
         node.insideZone = result;
@@ -25,6 +26,26 @@ export function truncateGraph (
 
     if (!hasNodesInsideZone)
         throw new RangeError('All graph nodes are located outside zone of interest.');
+
+    // inserting intersection nodes when path enters/exits zone
+    const headNodes = graph.topologicallySortedNodes().filter((node) => graph.indegreeOfNode(node.hash) === 0);
+    const pathGraphs = headNodes.map((headNode: PointNode) => graph.getSubGraphStartingFrom(headNode.hash));
+    for (const graph of pathGraphs) {
+        const nodes = graph.topologicallySortedNodes();
+        for (let i=1; i<nodes.length-1; i++) {
+            const previousNode = nodes[i-1];
+            const currentNode = nodes[i];
+
+            if (!previousNode.insideZone && currentNode.insideZone) {
+                // TODO insert intersection node between previous and current nodes
+            } else if (previousNode.insideZone && !currentNode.insideZone) {
+                // TODO insert intersection node between previous and current nodes
+            } else if (!previousNode.insideZone && !currentNode.insideZone) {
+                // TODO check if there are zone intersections
+                // TODO add them all between previous and current nodes
+            }
+        }
+    }
 
     return graph;
 }
